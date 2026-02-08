8a340db02eee4a212fa084838c2270c0373b89fc
Amn3sia <ayamareaper@gmail.com>
2026-02-08 17:07:09 +0300

    Add enhanced chat view and IRIR demo

--- modern_tui/__init__.py
+++ modern_tui/__init__.py
@@ -3,7 +3,34 @@
 Expose commonly used helpers for convenient imports in other modules.
 """
 from .utils import extract_youtube_id, sanitize_id
-from . import workers, compare, conversations, smoke
+from . import (
+    workers,
+    compare,
+    conversations,
+    smoke,
+    progress,
+    progress_enhanced,
+    sidebar,
+    welcome_screen,
+    enhanced_chat_view,
+)
 from .app import AIClient
+from .progress import AnimatedProgressBar, PureProgressBar, ProgressStyle
 
-__all__ = ["extract_youtube_id", "sanitize_id", "workers", "compare", "conversations", "smoke", "AIClient"]
+__all__ = [
+    "extract_youtube_id",
+    "sanitize_id",
+    "workers",
+    "compare",
+    "conversations",
+    "smoke",
+    "progress",
+    "progress_enhanced",
+    "sidebar",
+    "welcome_screen",
+    "enhanced_chat_view",
+    "AIClient",
+    "AnimatedProgressBar",
+    "PureProgressBar",
+    "ProgressStyle",
+]
